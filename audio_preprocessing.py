08259af6412588938a05cb63d1d30b2a104f6230
Myles <80717771+myl15@users.noreply.github.com>
2023-11-07 14:36:44 -0700

    Update audio_preprocessing.py
    Add comments and remove old code

--- audio_preprocessing.py
+++ audio_preprocessing.py
@@ -10,23 +10,23 @@ import numpy as np
 import re
 
 
+# A function to playback the audio
+def playback(file_to_open):
+	sound = pydub.AudioSegment.from_wav(file_to_open)
+	playback = simpleaudio.play_buffer(
+		sound.raw_data,
+		num_channels=sound.channels,
+		bytes_per_sample=sound.sample_width,
+		sample_rate=sound.frame_rate
+	)
+	time.sleep(5)
+	playback.stop()
 
-
-
-
-# sound = pydub.AudioSegment.from_wav(file_to_open)
-# playback = simpleaudio.play_buffer(
-# 	sound.raw_data,
-# 	num_channels=sound.channels,
-# 	bytes_per_sample=sound.sample_width,
-# 	sample_rate=sound.frame_rate
-# )
-# time.sleep(3)
-# playback.stop()
-
+# A function to remove silence
 def removeSilence(signal):
 	return signal[librosa.effects.split(signal)[0][0] : librosa.effects.split(signal)[0][-1]]
 
+# A function to create same-sized chunks of audio
 def create_chunks(audio, filename):
 	if len(audio) < 10000:
 		return "Audio too small"
@@ -42,7 +42,13 @@ def create_chunks(audio, filename):
 		curr_chunk.export(out_f = filename + '-' + str(chunk_no) + '.wav', format="wav")
 		t1, t2 = t2, t1
 
-def mel_spectogram_generator(audio_name, signal, sample_rate, augmentation, target_path):
+# A function to display a waveplot of the original audio
+def displayWavePlot(signal, sample_rate):
+	librosa.display.waveshow(signal,sr=sample_rate)
+	plt.show()
+	
+# A function to generate and potentially display the mel-spectrogram
+def mel_spectogram_generator(audio_name, signal, sample_rate, augmentation, target_path,create_figure):
 	N_FFT = 1028
 	HOP_SIZE = 256
 	N_MELS = 128
@@ -55,40 +61,28 @@ def mel_spectogram_generator(audio_name, signal, sample_rate, augmentation, targ
 	spectrogram = np.abs(mel_signal)
 	output_spectrogram = librosa.power_to_db(spectrogram, ref=np.max)
 
-
-	# plt.figure(figsize=(8,7))
-	# librosa.display.specshow(output_spectrogram, sr=sample_rate, x_axis='time',y_axis='mel',cmap='magma',hop_length=HOP_SIZE)
-	# plt.colorbar(label='Amplitude')
-	# plt.title('Mel-Spectrogram (dB)', fontdict=dict(size=18))
-	# plt.xlabel('Time', fontdict=dict(size=15))
-	# plt.ylabel('Frequency', fontdict=dict(size=15))
-	# # plt.show()
-	#
-	# plt.savefig(os.path.join(target_path + augmentation + audio_name[:-4] + '.png'))
-	# plt.clf()
-	# plt.close("all")
+	if create_figure:
+		plt.figure(figsize=(8,7))
+		librosa.display.specshow(output_spectrogram, sr=sample_rate, x_axis='time',y_axis='mel',cmap='magma',hop_length=HOP_SIZE)
+		plt.colorbar(label='Amplitude')
+		plt.title('Mel-Spectrogram (dB)', fontdict=dict(size=18))
+		plt.xlabel('Time', fontdict=dict(size=15))
+		plt.ylabel('Frequency', fontdict=dict(size=15))
+		# plt.show()
+		
+		plt.savefig(os.path.join(target_path + augmentation + audio_name[:-4] + '.png'))
+		plt.clf()
+		plt.close("all")
 	return np.mean(librosa.feature.mfcc(S=output_spectrogram, sr=sample_rate, n_mfcc=20).T,axis=0).tolist()
 
-# for wav in os.listdir(data_folder):
-# 	print(wav)
-# 	# The regular expression pattern
-# 	pattern = r'\.txt'
-
-# 	# Find all occurrences of the pattern
-# 	matches = re.findall(pattern, wav)
-
-# 	if not matches:
-# 		audio = pydub.AudioSegment.from_wav(data_folder.as_posix() +"/" + wav)
-# 		create_chunks(audio, wav[:-4])
-
-# times = np.arange(0,len(signal)) / sample_rate
-# plt.plot(times, signal)
+# The preprocessing function that should be called by other methods
 def preprocessing(data_folder, file_to_open):
-	# data_folder = Path(data_folder)
-	# file_to_open = data_folder / file_to_open
+	# Use regex to get the correct class name for the file
 	p = re.compile('\\\\[^sound_files][a-zA-z_a-zA-Z]*$')
 	augmentation = p.search(str(data_folder)).group()
 	augmentation = augmentation[1:]
+
+	# Some of the files don't work so it is wrapped in a try-catch block
 	try:
 		signal, sample_rate = librosa.load(file_to_open, sr=None)
 	except Exception as e:
@@ -101,17 +95,12 @@ def preprocessing(data_folder, file_to_open):
 			new_path = os.path.join("datasets/bad-sounds", augmentation)
 			os.rename(file_to_open, new_path)
 		raise Exception("Bad File")
-	# print(signal, sample_rate)
-	#
-	# librosa.display.waveshow(signal,sr=sample_rate)
-	# plt.show()
 
-	# D = librosa.amplitude_to_db(np.abs(librosa.stft(signal)), ref=np.max)
+	# If we ever need to create chunks in the future, this code will do it
+		#create_chunks(signal, augmentation)
 
-	#create_chunks(signal, augmentation)
-	return (mel_spectogram_generator('out',signal,sample_rate,augmentation,os.path.join(data_folder, 'output')))
-# amplitude to db converts and amplitude spectrogram to db-scaled spectrogram
-# librosa.display.specshow(D, y_axis='linear')
-# plt.colorbar(format='%+2.0f dB')
-# plt.title('Linear-frequency Power Spectrogram')
-# plt.show()
+	# Remove silence from the file
+	signal = removeSilence(signal)
+
+	# Create the mel-spectrogram vector
+	return (mel_spectogram_generator('out',signal,sample_rate,augmentation,os.path.join(data_folder, 'output'), False))
