87328ae05cb334d05d59fa83aef088538453805c
prestonraab <prestonraab@gmail.com>
2023-11-20 10:45:16 -0700

    Fix prediction labels

--- model-training.py
+++ model-training.py
@@ -28,8 +28,9 @@ def createCSV(trainingPath: Path):
             continue
 
         # Add the expected output
-        instrument = str(sound_file).split('-')[0]
+        instrument = str(sound_file.name).split('-')[0]
         processed_file.append(instrument)
+        processed_file.append(sound_file.name)
 
         # Add the complete data instance to one large DataFrame
         frame = pd.concat([frame, pd.DataFrame([processed_file])], ignore_index=True)
@@ -43,19 +44,20 @@ def main(create_CSV: bool, training_data_path: Path):
     ### Train-test-split
     frame = pd.read_csv('training_data.csv')
     enc = preprocessing.LabelEncoder()
-    y = frame.iloc[:,20]
+    y = frame.iloc[:, -2]
+    f_names = frame.iloc[:, -1]
     y = y.values.reshape(-1, 1)
     y = y.ravel()
     y = enc.fit_transform(y)
-    X = frame.drop(frame.columns[-1], axis=1)
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, shuffle=True)
+    X = frame.drop(frame.columns[-2:], axis=1)
+    X_train, X_test, y_train, y_test, f_names_train, f_names_test = train_test_split(X, y, f_names, test_size=0.2, shuffle=True)
 
 
     ### Train the model and validate
     try:
         clf = load('triple_model.joblib')
     except Exception as e:
-        clf = MLPClassifier(hidden_layer_sizes=[25,25],activation='relu',max_iter=2000,warm_start=False)
+        clf = MLPClassifier(hidden_layer_sizes=[25, 25], activation='relu',max_iter=2000, warm_start=False)
 
     # Train
     clf.fit(X_train.values, y_train)
@@ -65,12 +67,13 @@ def main(create_CSV: bool, training_data_path: Path):
     test_acc = clf.score(X_test.values, y_test)
     predictions = pd.DataFrame(enc.inverse_transform(clf.predict(X_test.values)))
     predictions['Actual'] = enc.inverse_transform(y_test)
+    predictions['Filename'] = f_names_test.values
     best_loos = clf.best_loss_
     num_iter = clf.n_iter_
 
     # Put all the values into a table and write the tables to .csv files
-    table = pd.DataFrame({'Train Accuracy' : [train_acc], 'Test Accuracy' : [test_acc], 'Best Loss' : [best_loos], 'Number Interations' : [num_iter]})
-    predictions.to_csv('triple_predictions.csv',index=False)
+    table = pd.DataFrame({'Train Accuracy': [train_acc], 'Test Accuracy': [test_acc], 'Best Loss': [best_loos], 'Number Iterations': [num_iter]})
+    predictions.to_csv('triple_predictions.csv', index=False)
     table.to_csv('triple_model_results.txt', mode='a', index=False)
 
     # Save the current state of the model
@@ -84,7 +87,7 @@ def main(create_CSV: bool, training_data_path: Path):
 
 if __name__ == '__main__':
     argparser = argparse.ArgumentParser(description='Train Model')
-    argparser.add_argument('create_CSV', type=bool, help='Is a new CSV File needed?')
+    argparser.add_argument('-c', '--create_CSV', action="store_true", help='Is a new CSV File needed?')
     argparser.add_argument('training_folder', type=str, help='Path to training data')
     args = argparser.parse_args()
     main(bool(args.create_CSV), Path(args.training_folder))
